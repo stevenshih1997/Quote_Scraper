6a657612fb0c7efe592754c74bcc1949bc5ef745
Steven Shih <stevenshih1997@gmail.com>
2017-07-31 00:46:21 -0700

    optimized quote scraper

--- quote_scraper.py
+++ quote_scraper.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup, SoupStrainer
 import keywords
 import utils
 import progressbar
+
 class ScrapeKeyword(object):
     """docstring for ScrapeKeyword"""
     def __init__(self, keyword, num_pages):
@@ -22,7 +23,7 @@ class ScrapeKeyword(object):
     def scrape_first_page(self, result):
         """Initializes scraping and gets first url page based on search word"""
         response = requests.get(self.url)
-        parse_only = SoupStrainer(id='quotesList')
+        parse_only = SoupStrainer(title=['view quote','view author'])
         soup = BeautifulSoup(response.content, 'lxml', parse_only=parse_only)
         result.append(soup) #soup.select('#quotesList a')
         #return result
@@ -31,7 +32,7 @@ class ScrapeKeyword(object):
         """Scrape brainyquotes based on search result multithreaded"""
         try:
             response = requests.post(self.url, params=params) # Get all search results
-            parse_only = SoupStrainer(id='quotesList')
+            parse_only = SoupStrainer(title=['view quote','view author'])
             soup = BeautifulSoup(response.content, 'lxml', parse_only=parse_only)
             #with self.lock:
             results.append(soup)#soup.select('#quotesList a')
@@ -41,6 +42,8 @@ class ScrapeKeyword(object):
     def run_queue_page(self, que, soup):
         while True:
             param = que.get()
+            if param is None:
+                break
             self.multi_scrape_quotes(soup, param)
             que.task_done()
 
@@ -49,15 +52,16 @@ class ScrapeKeyword(object):
         results = []
         self.scrape_first_page(results)
         parameters = ({"typ":"search", "langc":"en", "ab":"b", "pg":page, "id":self.keyword, "m":0} for page in range(self.num_pages))
-        num_threads = 8
+        num_threads = 5
         pg_que = Queue()
         for param in parameters:
             pg_que.put(param)
         #threads = (threading.Thread(target=self.multi_scrape_quotes, args=(page, results, )) for page in range(2, self.num_pages + 1))
         for _ in range(num_threads):
             worker = threading.Thread(target=self.run_queue_page, args=(pg_que, results))
+            worker.daemon = True
             worker.start()
-            time.sleep(.1)
+            time.sleep(.01)
         pg_que.join()
         return results
 
@@ -86,36 +90,37 @@ def scrape_all(scrape_list, page_range):
     if len(scrape_list) < 20:
         num_threads = len(scrape_list)
     else:
-        num_threads = 15 #optimal number of threads
+        num_threads = 25 #optimal number of threads
     que = Queue()
     all_results = []
     for topic in scrape_list:
         que.put(topic)
     for _ in range(num_threads):
         worker = threading.Thread(target=run_queue_author_quote, args=(que, all_results, page_range))
-        #worker.daemon = True
+        worker.daemon = True
         worker.start()
-        time.sleep(1)
+        time.sleep(.5)
         #progress(i, num_threads, status=' Scraping...')
     que.join()
     return format_quotes(all_results)
 
-def format_quotes(quotes):
+def format_quotes(quotes_list):
     """Format quotes to a dict"""
-    quotes_list = []
-    authors_list = []
+    quotes_list_result = []
+    authors_list_result = []
     result = defaultdict(set)
-    for quote in quotes:
+    
+    for quote in quotes_list:
         for q in quote.find_all(title='view quote'):
-            quotes_list.append(q.text)
+            quotes_list_result.append(q.text)
         for q in quote.find_all(title='view author'):
-            authors_list.append(q.text)
+            authors_list_result.append(q.text)
         # new_quote = quote.find_all('a')
         # if new_quote.get('title') == 'view quote':
         #     quotes_list.append(new_quote.text)
         # if new_quote.get('title') == 'view author':
         #     authors_list.append(new_quote.text)
-    for author, quote in zip(authors_list, quotes_list):
+    for author, quote in zip(authors_list_result, quotes_list_result):
         result[author].add(quote)
     result = dict(result)
     for key, value in result.items():
@@ -127,8 +132,10 @@ if __name__ == '__main__':
     FILENAME = sys.argv[1]
     START = time.time()
     spinner = progressbar.Spinner()
+    print('Scraping... ')
     spinner.start()
     utils.output_json(scrape_all(ALL_TOPICS, 20), FILENAME) if utils.validate_name(FILENAME) else sys.exit()
     END = time.time()
     spinner.stop()
+    print('Done!')
     print('Time taken to scrape: {0:.2f} seconds'.format(END - START))
--- sqlite_db.py
+++
@@ -1,8 +0,0 @@
-"""SQLite3 database operations for quotes"""
-import sqlite3
-
-class DatabaseOps(object):
-    """docstring for DatabaseOps"""
-    def __init__(self, keyword):
-        super(DatabaseOps, self).__init__()
-        self.keyword = keyword
